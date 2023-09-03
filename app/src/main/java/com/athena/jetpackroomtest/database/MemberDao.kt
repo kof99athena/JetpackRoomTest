600512b3daaf86b2fbeae3cf4a7bcc6257c789e4
kof99athena <kof99athena@gmail.com>
2023-09-03 23:45:14 +0900

    Room 이해하기

--- app/src/main/java/com/athena/jetpackroomtest/database/MemberDao.kt
+++ app/src/main/java/com/athena/jetpackroomtest/database/MemberDao.kt
@@ -14,6 +14,8 @@ import androidx.room.Insert
 @Dao
 interface MemberDao{
     @Insert
+    //인서트는 엔티티에 set을 삽입하게되고, 엔티티의 어노테이션에 정의된 class만 인자로 받는다.
+    //충돌 정책도 잡을 수 있다.
     fun insert(name:PsTeam)
     //PsTeam에 name 데이터 추가
 
@@ -21,3 +23,20 @@ interface MemberDao{
     fun delete(name:PsTeam)
     //PsTeam에 name 데이터 삭제
 }
+
+//다오는 데이터베이스에 접근하여 실행할 작업을 메소드형테로 정의한다.
+//SQL쿼리를 지정가능하다
+//@Update,@Query 등등
+//@Query는 쿼리를 사용해서 DB를 조회 할 수 있다. 컴파일 타임에 리턴되는 오브젝트의 필드와
+//SQL 결과로 나온 컬럼의 이름이 맞는지 여부를 확인해서
+//맞으면 워닝, 아니면 에러를 보낸다.
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/athena/jetpackroomtest/database/PsTeam.kt
+++ app/src/main/java/com/athena/jetpackroomtest/database/PsTeam.kt
@@ -1,5 +1,6 @@
 package com.athena.jetpackroomtest.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,7 +8,7 @@ import androidx.room.PrimaryKey
 //테이블을 만들자. 사이코솔저팀의 프로필을 보여주고싶다. 이름/국적/기술
 //어노테이션쓰고싶다면 gradle에 추가해놓고 사용해야함.
 
-@Entity
+@Entity //tableName을 설정할 수 있다.
 data class PsTeam(
     var name : String,
     var nation : String,
@@ -16,5 +17,13 @@ data class PsTeam(
     @PrimaryKey(autoGenerate = true) var id: Int = 0
     //프라이머리키를 다른거로 할순없을까?
     //프라이머리키가 이해안감
+    //autogenerate하면 자동증가함
 
 }
+
+//엔티티란?
+//DB에 저장할 데이터 형식으로 class의 변수를 지정한다.
+//tableName을 설정할 수 있다. 선언하지않으면 class이름으로 인식한다.
+//@PrimaryKey : 각 엔티티마다 1개 이상을 갖고 있어야한다.
+
+//@ColumnInfo : table내의 컬럼변수에 매칭시켜준다.
